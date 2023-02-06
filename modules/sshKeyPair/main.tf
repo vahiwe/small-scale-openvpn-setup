ba5600ecc188f5cf9f4f042370e5f2a696329e8a
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2023-02-06 20:28:46 +0100

    Setup sshKeyPair Module

---
+++ modules/sshKeyPair/main.tf
@@ -0,0 +1,17 @@
+resource "tls_private_key" "dev_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.generated_key_name
+  public_key = tls_private_key.dev_key.public_key_openssh
+
+  provisioner "local-exec" { # Generate "terraform-key-pair.pem" in current directory
+    command = <<-EOT
+      echo '${tls_private_key.dev_key.private_key_pem}' > ./'${var.generated_key_name}'.pem
+      chmod 400 ./'${var.generated_key_name}'.pem
+    EOT
+  }
+
+}
