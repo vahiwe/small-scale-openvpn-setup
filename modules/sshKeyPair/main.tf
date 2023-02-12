f9f342a22e766565934c9b754a611e01e229ab58
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2023-02-12 21:54:10 +0100

    fixup! Update sshKeyPair Module

--- modules/sshKeyPair/main.tf
+++ modules/sshKeyPair/main.tf
@@ -6,12 +6,10 @@ resource "tls_private_key" "dev_key" {
 resource "aws_key_pair" "generated_key" {
   key_name   = var.generated_key_name
   public_key = tls_private_key.dev_key.public_key_openssh
+}
 
-  provisioner "local-exec" { # Generate "terraform-key-pair.pem" in current directory
-    command = <<-EOT
-      echo '${tls_private_key.dev_key.private_key_pem}' > ${var.save_path}/'${var.generated_key_name}'.pem
-      chmod 400 ${var.save_path}/'${var.generated_key_name}'.pem
-    EOT
-  }
-
+resource "local_file" "private_key" {
+  content         = tls_private_key.dev_key.private_key_pem
+  filename        = "${var.save_path}/${var.generated_key_name}.pem"
+  file_permission = "0400"
 }
